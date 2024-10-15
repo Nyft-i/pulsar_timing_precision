c1879452366c2ccfdbca0927c634a28183cbd459
Jade Salisbury <thomas.salisbury-2@student.manchester.ac.uk>
2024-10-15 12:37:43 +0100

    updated new file

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -5,3 +5,54 @@ Created on Tue Oct 15 12:33:34 2024
 @author: jade
 """
 
+import tim_sampling
+import numpy as np
+
+timfile = "master_toas.tim"
+toas = np.genfromtxt(timfile, skip_header=1, usecols=[2])
+
+# User changeable 
+# 'logarithmic', 'arithmetic', 'geometric', 'periodic'
+SEQUENCE_TYPE = 'periodic'
+cadence_start = 0.5
+marker_offset = 0
+max_gap = 20
+verbose = True
+
+## LOGARITHMIC - 
+# these parameters are only used if SEQUENCE_TYPE is 'logarithmic'
+log_const = 0.4
+
+## ARITHMETIC - 
+# these parameters are only used if SEQUENCE_TYPE is 'arithmetic'
+sequential_increase = 2 # num. of days per observation that time between observations increases by
+
+## GEOMETRIC - 
+# these parameters are only used if SEQUENCE_TYPE is 'geometric'
+multiplicative_increase = 2 # factor time between observations is multiplied by after an observation
+
+## PERIODIC - 
+# these parameters are only used if SEQUENCE_TYPE is 'periodic'
+period = 1
+
+if SEQUENCE_TYPE == 'logarithmic':
+    args = (cadence_start, marker_offset, max_gap, log_const)
+    indexes = tim_sampling.sample_from_toas(toas, 'logarithmic', args, verbose)
+elif SEQUENCE_TYPE == 'arithmetic':
+    args = (cadence_start, marker_offset, max_gap, sequential_increase)
+    indexes = tim_sampling.sample_from_toas(toas, 'arithmetic', args, verbose)
+elif SEQUENCE_TYPE == 'geometric':
+    args = (cadence_start, marker_offset, max_gap, multiplicative_increase)
+    indexes = tim_sampling.sample_from_toas(toas, 'geometric', args, verbose)
+elif SEQUENCE_TYPE == 'periodic':
+    args = (cadence_start, marker_offset, max_gap, period)
+    indexes = tim_sampling.sample_from_toas(toas, 'periodic', args, verbose)
+else:
+    print("invalid sequence type. doing nothing.")    
+
+
+print("number of toas: " + str(len(indexes)))
+
+new_filename = SEQUENCE_TYPE + "_toas.tim"
+tim_sampling.gen_new_tim(timfile, indexes, new_filename)
+
