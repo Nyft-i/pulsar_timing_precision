f12e145cbc1c7ac34c8b11e2864e7d0495925b21
lauren-alt <lauren.maclean@student.manchester.ac.uk>
2024-10-16 14:50:14 +0100

    changed how call find peaks

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -11,6 +11,7 @@ import subprocess
 import pandas
 import matplotlib.pyplot as plt
 import scipy as sp
+from scipy.signal import find_peaks
 
 def compare_to_master(par, master_traits):
     # f0 % diff
@@ -73,7 +74,7 @@ def simulate(toas, sequence_type, const_args, sim_args):
         plt.plot(x,np.abs(y))
         plt.xlabel("log constant")
         plt.ylabel("absolute value of percentage difference of retrieved and actual GLF0")
-        min_constant = sp.signal.find_peaks(-y, distance= 1000)
+        min_constant = find_peaks(-y, distance= 1000)
         print(y[min_constant])
         plt.scatter(x[min_constant],y[min_constant], marker="x")
         plt.savefig("results_15_20_24.png", dpi=400)
@@ -95,7 +96,7 @@ verbose = False
 #simulation parameters
 log_const_min = 0
 log_const_max = 3.8
-num_iterations = 100
+num_iterations = 10
 
 ## LOGARITHMIC - 
 # these parameters are only used if SEQUENCE_TYPE is 'logarithmic'
