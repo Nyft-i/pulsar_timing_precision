5ec271e10e82e5d416df1f1fcfbf172e7f79a1b2
Jade Salisbury <thomas.salisbury-2@student.manchester.ac.uk>
2024-10-17 13:55:57 +0100

    adding all other simulation types

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -58,100 +58,108 @@ def run_fit(par, tim):
 
 def simulate(toas, sequence_type, const_args, sim_args):
     curr_iter = 0
-    curr_log_const = sim_args[0]
+    curr_sim_const = sim_args[0]
     step = np.abs(sim_args[1] - sim_args[0])/(sim_args[2]-1)
     master_par = "master_file.par"
     cols = ["Element Name", "Value", "Fitting", "Error"]
     master_properties = pandas.read_csv(master_par, sep="\s+", names=cols)
     master_traits = float(master_properties.loc[master_properties['Element Name'] == "GLF0_1"]['Value']), float(master_properties.loc[master_properties['Element Name'] == "GLF1_1"]['Value']), float(master_properties.loc[master_properties['Element Name'] == "GLPH_1"]['Value'])
 
-    
-    if sequence_type == 'logarithmic':
-        results = np.zeros((0,5))
-        while curr_iter<sim_args[2]:
-            curr_iter += 1
-            #print(toas)
-            #print(indexes)
-            # adds some 5d random variation so that we dont run into issues with the sample being the same every time
-            start_randomiser = np.array([(const_args[1] + random.randint(0, 50)/10),
-                                (const_args[1] + random.randint(0, 50)/10),
-                                (const_args[1] + random.randint(0, 50)/10)])
-            for offset in start_randomiser:
-                passed_args = const_args[0], const_args[1]+offset, const_args[2], curr_log_const
-                indexes = tim_sampling.sample_from_toas(toas, sequence_type, passed_args)
-                print("index array made")   
-                new_filename = sequence_type + "_toas.tim"
-                num_toas = tim_sampling.gen_new_tim(timfile, indexes, new_filename)
-                print("new toas generated, running tempo2")
-    
-                # run tempo2
-                par, tim = "master_file_noglitch.par", new_filename
-                traits = run_fit(par, tim)
-                #print(traits)
+
+    results = np.zeros((0,5))
+    while curr_iter<sim_args[2]:
+        curr_iter += 1
+        #print(toas)
+        #print(indexes)
+        # adds some 5d random variation so that we dont run into issues with the sample being the same every time
+        start_randomiser = np.array([(const_args[1] + random.randint(0, 50)/10),
+                            (const_args[1] + random.randint(0, 50)/10),
+                            (const_args[1] + random.randint(0, 50)/10)])
+        
+        for offset in start_randomiser:
+            passed_args = const_args[0], const_args[1]+offset, const_args[2], curr_sim_const
+            indexes = tim_sampling.sample_from_toas(toas, sequence_type, passed_args)
+
+            
+            
+            
+            
                 
-                
-                print("retrieving results")
-                compare = compare_to_master(traits, master_traits)
-                #print(compare)
-                curr_results = curr_log_const, compare[0], compare[1], compare[2], num_toas
-                results = np.vstack((results, curr_results))
-                
-            print("successfully simulated #"+ str(curr_iter)+ ", stepping log_const by "+str(step))
-            curr_log_const += step
-            print("(log_const is now "+str(curr_log_const)+")")
-        # Below are settings used to generate a graphh.
-        results = results.astype('float64')
-        #print(results)
-        x = results[:,0].astype('float64')
-        y = results[:,1].astype('float64')
-        #z = np.log(results[:,4])
-        #scaled_z = (z - z.min()) / z.ptp()
-        #colours = plt.cm.Greys(scaled_z)
-        plt.scatter(x,np.abs(y),cmap='gist_gray',c=results[:,4],norm=colors.LogNorm(),edgecolors='gray')
-        plt.colorbar(label="Num. of ToAs")
-        plt.xlabel("log constant")
-        plt.ylabel("absolute value of % diff of retrieved and actual GLF0")
-        minimum = find_peaks(-np.abs(y), distance= 2000)
-        min_constant = minimum[0]
-        print(y[min_constant])
-        plt.scatter(x[min_constant],np.abs(y[min_constant]), marker="x", color = "red")
-        plt.tight_layout()
-        plt.savefig("results_15_20_24.png", dpi=400)
-        return
-    else:
-        print("you should never see this")
+            print("index array made")   
+            new_filename = "temp_toas.tim"
+            
+            
+            num_toas = tim_sampling.gen_new_tim(timfile, indexes, new_filename)
+            print("new toas generated, running tempo2")
+
+            # run tempo2
+            par, tim = "master_file_noglitch.par", new_filename
+            traits = run_fit(par, tim)
+            #print(traits)
+            
+            
+            print("retrieving results")
+            compare = compare_to_master(traits, master_traits)
+            #print(compare)
+            curr_results = curr_sim_const, compare[0], compare[1], compare[2], num_toas
+            results = np.vstack((results, curr_results))
+            
+        print("successfully simulated #"+ str(curr_iter)+ ", stepping log_const by "+str(step))
+        curr_sim_const += step
+        print("the "+sequence_type+"_const is now "+str(curr_sim_const)+")")
+    # Below are settings used to generate a graphh.
+    results = results.astype('float64')
+    #print(results)
+    x = results[:,0].astype('float64')
+    y = results[:,1].astype('float64')
+    #z = np.log(results[:,4])
+    #scaled_z = (z - z.min()) / z.ptp()
+    #colours = plt.cm.Greys(scaled_z)
+    plt.scatter(x,np.abs(y),cmap='gist_gray',c=results[:,4],norm=colors.LogNorm(),edgecolors='gray')
+    plt.colorbar(label="num. of ToAs")
+    plt.xlabel(sequence_type+" constant")
+    plt.ylabel("absolute value of % diff of retrieved and actual GLF0")
+    minimum = find_peaks(-np.abs(y), distance= 2000)
+    min_constant = minimum[0]
+    print(y[min_constant])
+    plt.scatter(x[min_constant],np.abs(y[min_constant]), marker="x", color = "red")
+    plt.tight_layout()
+    plt.savefig("results_15_20_24.png", dpi=400)
+    return
     
 timfile = "master_toas_2.tim"
 toas = np.genfromtxt(timfile, skip_header=1, usecols=[2])
 
 # User changeable 
 # 'logarithmic', 'arithmetic', 'geometric', 'periodic'
-SEQUENCE_TYPE = 'logarithmic'
+SEQUENCE_TYPE = 'geometric'
 cadence_start = 0.5
 marker_offset = 0
 max_gap = 20
 verbose = False
 
 #simulation parameters
-log_const_min = 0
-log_const_max = 3
-num_iterations = 250
+num_iterations = 50
 
 ## LOGARITHMIC - 
 # these parameters are only used if SEQUENCE_TYPE is 'logarithmic'
-log_const = 0.4
+log_const_min = 0
+log_const_max = 3
 
 ## ARITHMETIC - 
 # these parameters are only used if SEQUENCE_TYPE is 'arithmetic'
-sequential_increase = 2 # num. of days per observation that time between observations increases by
+sequential_increase_min = 2 # num. of days per observation that time between observations increases by
+sequential_increase_max = 2 # num. of days per observation that time between observations increases by
 
 ## GEOMETRIC - 
 # these parameters are only used if SEQUENCE_TYPE is 'geometric'
-multiplicative_increase = 2 # factor time between observations is multiplied by after an observation
+multiplicative_increase_min = 1.2 # factor time between observations is multiplied by after an observation
+multiplicative_increase_max = 4 # factor time between observations is multiplied by after an observation
 
 ## PERIODIC - 
 # these parameters are only used if SEQUENCE_TYPE is 'periodic'
-period = 1
+period_min = 1
+period_max = 1
 
 
 if SEQUENCE_TYPE == 'logarithmic':
@@ -159,14 +167,17 @@ if SEQUENCE_TYPE == 'logarithmic':
     sim_args = (log_const_min, log_const_max, num_iterations)
 #    indexes = tim_sampling.sample_from_toas(toas, 'logarithmic', args, verbose)
 elif SEQUENCE_TYPE == 'arithmetic':
-    args = (cadence_start, marker_offset, max_gap, sequential_increase)
- #   indexes = tim_sampling.sample_from_toas(toas, 'arithmetic', args, verbose)
+    const_args = (cadence_start, marker_offset, max_gap)
+    sim_args = (log_const_min, log_const_max, num_iterations)
+#    indexes = tim_sampling.sample_from_toas(toas, 'arithmetic', args, verbose)
 elif SEQUENCE_TYPE == 'geometric':
-    args = (cadence_start, marker_offset, max_gap, multiplicative_increase)
-  #  indexes = tim_sampling.sample_from_toas(toas, 'geometric', args, verbose)
+    const_args = (cadence_start, marker_offset, max_gap)
+    sim_args = (multiplicative_increase_min, multiplicative_increase_max, num_iterations)
+#    indexes = tim_sampling.sample_from_toas(toas, 'geometric', args, verbose)
 elif SEQUENCE_TYPE == 'periodic':
-    args = (cadence_start, marker_offset, max_gap, period)
-   # indexes = tim_sampling.sample_from_toas(toas, 'periodic', args, verbose)
+    const_args = (cadence_start, marker_offset, max_gap)
+    sim_args = (log_const_min, log_const_max, num_iterations)
+#    indexes = tim_sampling.sample_from_toas(toas, 'periodic', args, verbose)
 else:
     print("invalid sequence type. doing nothing.")    
 
