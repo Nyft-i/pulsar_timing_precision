ed7d23025b792b37e19a794a74a7f850622311fc
lauren-alt <lauren.maclean@student.manchester.ac.uk>
2024-10-22 09:51:16 +0100

    plotting errors

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -113,12 +113,12 @@ def simulate(toas, sequence_type, const_args, sim_args):
     #scaled_z = (z - z.min()) / z.ptp()
     #colours = plt.cm.Greys(scaled_z)
     
-    #plt.errorbar(x,np.abs(y),yerr=y_err,fmt=',')
+    plt.errorbar(x,np.abs(y),yerr=y_err,fmt=',')
     plt.scatter(x,np.abs(y),cmap='gist_gray',c=results[:,6],norm=colors.LogNorm(),edgecolors='gray')
     
     plt.colorbar(label="num. of ToAs")
     plt.xlabel(sequence_type+" constant")
-    plt.ylabel("absolute value of % diff of retrieved and actual GLF1")
+    plt.ylabel("absolute value of % diff of retrieved and actual GLF0")
     minimum = find_peaks(-np.abs(y), distance= 2000)
     min_constant = minimum[0]
     print(y[min_constant])
