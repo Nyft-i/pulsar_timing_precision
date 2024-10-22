b56fd4339689c4000a41fda7092a7f621bbc4077
lauren-alt <lauren.maclean@student.manchester.ac.uk>
2024-10-22 14:30:38 +0100

    fixed mid point calculation

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -114,7 +114,7 @@ def simulate(toas, sequence_type, const_args, sim_args):
             print(residuals[4,1])
             while counter <= len(residuals):
                 if np.abs(residuals[counter,1] - residuals[(counter -1),1]) < 10 * error:
-                    mid_point = (residuals[counter,1] + residuals[(counter -1),1])/2
+                    mid_point = (residuals[counter,0] + residuals[(counter -1),0])/2
                     print(mid_point)
                     break 
                     
