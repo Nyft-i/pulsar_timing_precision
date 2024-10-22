797910012e1cf2dbe83ded0337604168607232eb
Jade Salisbury <thomas.salisbury-2@student.manchester.ac.uk>
2024-10-22 12:08:21 +0100

    reducing the numb iters

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -141,7 +141,7 @@ max_gap = 20
 verbose = False
 
 #simulation parameters
-num_iterations = 50
+num_iterations = 1
 
 ## LOGARITHMIC - 
 # these parameters are only used if SEQUENCE_TYPE is 'logarithmic'
