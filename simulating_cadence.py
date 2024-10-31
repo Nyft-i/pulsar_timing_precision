d3e5ae9314e7e528065f3b3f196d6a42b23c9d36
Jade Salisbury <tom@salisbury-email.com>
2024-10-31 15:01:02 +0000

    prints

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -497,17 +497,17 @@ def main():
     
     args = (0.5, 0, 20, 1.0991)
     print("numtoas of log", tim_sampling.sample_from_toas(toas, 'logarithmic', args, counting_mode=True)[1])
-    results = single_simulate(toas, 'logarithmic', (0.5, 0, 20), 1.0991, num_sps=10)
+    results = single_simulate(toas, 'logarithmic', (0.5, 0, 20), 1.0991, num_sps=100)
     plt.errorbar(results[0]-master_traits[0], results[2]-master_traits[1], xerr=results[1], yerr=results[3], fmt='x', label="logarithmic")
     
     args = (0.5, 0, 20, 1.6394)
     print("numtoas of geo", tim_sampling.sample_from_toas(toas, 'geometric', args, counting_mode=True)[1])
-    results = single_simulate(toas, 'logarithmic', (0.5, 0, 20), 1.6394, num_sps=10)
+    results = single_simulate(toas, 'logarithmic', (0.5, 0, 20), 1.6394, num_sps=100)
     plt.errorbar(results[0]-master_traits[0], results[2]-master_traits[1], xerr=results[1], yerr=results[3], fmt='x', label="geometric")
     
     args = (0.5, 0, 20, 5)
     print("numtoas of periodic", tim_sampling.sample_from_toas(toas, 'periodic', args, counting_mode=True)[1])
-    results = single_simulate(toas, 'periodic', (0.5, 0, 20), 5, num_sps=10)
+    results = single_simulate(toas, 'periodic', (0.5, 0, 20), 5, num_sps=100)
     plt.errorbar(results[0]-master_traits[0], results[2]-master_traits[1], xerr=results[1], yerr=results[3], fmt='x', label="periodic")
     
     
