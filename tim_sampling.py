afafee41a1db3217b427c957122309974b8d5556
Jade Salisbury <tom@salisbury-email.com>
2024-11-05 12:09:50 +0000

    backwards fixx

--- tim_sampling.py
+++ tim_sampling.py
@@ -131,7 +131,7 @@ def main():
     
     # Reads the .tim file and extracts the TOAs
     toas = np.genfromtxt(timfile, skip_header=1, usecols=[2])
-    indexes = sample_from_toas(toas, sequence_type, args, verbose=True)
+    indexes, num_toas = sample_from_toas(toas, sequence_type, args, verbose=True)
     gen_new_tim(timfile, indexes, "new.tim")
     
 
