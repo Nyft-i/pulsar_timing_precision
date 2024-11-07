ae121da24e51bfc8224d506b5c77f86695326ba2
lauren-alt <lauren.maclean@student.manchester.ac.uk>
2024-11-07 15:55:04 +0000

    clearing GLEP

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -55,7 +55,7 @@ def epoch_finder(par, tim, master_traits):
     error = 0.0001
     #finds estimation of glitch epoch
     while counter <= len(residuals):
-        if np.abs(residuals[counter,1] - residuals[(counter -1),1]) > 20 * error:
+        if np.abs(residuals[counter,1] - residuals[(counter -1),1]) > 10 * error:
             print(residuals[counter,1])
             print(residuals[counter-1,1])
             change = ((residuals[counter,0] + residuals[(counter -1),0])/2) 
@@ -219,6 +219,7 @@ def single_simulate(toas, sequence_type, const_args, sim_arg, verbose = False, m
             all_results = np.vstack((all_results, results))
             editting_par(par, 0, "GLF0_1")
             editting_par(par, 0, "GLF1_1")
+            editting_par(par, 0)
             
         print(str(number+1) + ".", end="")
         sys.stdout.flush()
