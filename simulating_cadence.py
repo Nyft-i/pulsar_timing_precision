bec89f9e54cd69e40dbf11d8e7464b8aca9e594a
lauren-alt <lauren.maclean@student.manchester.ac.uk>
2024-11-07 14:04:04 +0000

    printing midpoint

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -57,6 +57,7 @@ def epoch_finder(par, tim, master_traits):
         if np.abs(residuals[counter,1] - residuals[(counter -1),1]) > 10 * error:
             change = ((residuals[counter,0] + residuals[(counter -1),0])/2) 
             mid_point = change + master_traits[3]
+            print(mid_point)
             break 
             
         else :
