94a316da1540a9cf7d7556e43573e9f15b4493a5
Jade Salisbury <tom@salisbury-email.com>
2024-11-07 15:21:17 +0000

    25sigma

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -55,7 +55,7 @@ def epoch_finder(par, tim, master_traits):
     error = 0.0001
     #finds estimation of glitch epoch
     while counter <= len(residuals):
-        if np.abs(residuals[counter,1] - residuals[(counter -2),1]) > 20 * error:
+        if np.abs(residuals[counter,1] - residuals[(counter -2),1]) > 25 * error:
             change = ((residuals[counter,0] + residuals[(counter -1),0])/2) 
             mid_point = change + master_traits[3]
             plt.scatter(residuals[:,0], residuals[:,1])
