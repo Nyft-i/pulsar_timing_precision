05a8cba11b7cf6701b4ddc65be5d9a6a257037c6
lauren-alt <lauren.maclean@student.manchester.ac.uk>
2024-11-07 15:28:27 +0000

    upping the limit

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -55,7 +55,7 @@ def epoch_finder(par, tim, master_traits):
     error = 0.0001
     #finds estimation of glitch epoch
     while counter <= len(residuals):
-        if np.abs(residuals[counter,1] - residuals[(counter -2),1]) > 25 * error:
+        if np.abs(residuals[counter,1] - residuals[(counter -1),1]) > 50 * error:
             change = ((residuals[counter,0] + residuals[(counter -1),0])/2) 
             mid_point = change + master_traits[3]
             plt.scatter(residuals[:,0], residuals[:,1])
@@ -66,7 +66,7 @@ def epoch_finder(par, tim, master_traits):
             break 
             
         else :
-            counter += 2
+            counter += 1
     
     return mid_point
 
