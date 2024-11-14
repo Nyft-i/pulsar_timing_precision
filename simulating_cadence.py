23ea54549db9375cbbb0cb5493afffa74d884bc8
Jade Salisbury <tom@salisbury-email.com>
2024-11-14 17:20:30 +0000

    rec fit first

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -186,7 +186,7 @@ def single_simulate(toas, sequence_type, const_args, sim_arg, verbose = False, m
         editting_par(par, new_GLEP)
         
         # run tempo2
-        traits = run_fit(par, tim)
+        traits = run_fit(par, tim, recovery_mode=True)
         print(traits)
         editting_par(par, traits[0], "GLF0_1")
         editting_par(par, traits[2], "GLF1_1")
