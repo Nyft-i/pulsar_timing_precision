80d3ced32afd1eb6d1173165ce68898121ca5690
lauren-alt <lauren.maclean@student.manchester.ac.uk>
2024-11-19 14:55:42 +0000

    moreeee label adjustments

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -509,8 +509,8 @@ def diff_plot_recovery():
     axs = gs.subplots(sharey = True, sharex = True)
     
     fig.suptitle(r'difference in retrieved recovery portion of $\Delta \nu$ and $\tau_r$ and actual values', x=0.5, y=1.05)
-    fig.supylabel(r'distance from true recovery portion of $\Delta \nu$', y=0.47, x=0.08)
-    fig.supxlabel(r'distance from true $\tau_r$', y = 0.02)
+    fig.supylabel(r'distance from true recovery portion of $\Delta \nu$', y=0.45, x=0.08)
+    fig.supxlabel(r'distance from true $\tau_r$', y = -0.05)
     
     seq = 'logarithmic'
     const = 25.7197
@@ -585,8 +585,8 @@ def diff_plot_recovery():
     axs = gs.subplots(sharey = True, sharex = True)
     
     fig.suptitle(r'difference in retrieved $\Delta \nu$ and $\Delta \dot \nu$ and actual values', x=0.5, y=1.05)
-    fig.supylabel(r'distance from true $\Delta \dot \nu$', y=0.45, x=0.08)
-    fig.supxlabel(r'distance from true $\Delta \nu$', y = 0.02)
+    fig.supylabel(r'distance from true $\Delta \dot \nu$', y=0.4, x=0.08)
+    fig.supxlabel(r'distance from true $\Delta \nu$', y = -0.05)
     
     axs[0].scatter(all_results_log[:,1]-master_traits[0], all_results_log[:,3]-master_traits[1], facecolors='none', edgecolors='tab:blue', s=all_results_log[:,7]*25, zorder=10, alpha = 0.3)
     axs[0].errorbar(all_results_log[:,1]-master_traits[0], all_results_log[:,3]-master_traits[1], xerr=all_results_log[:,2], yerr=all_results_log[:,4], fmt='x', label=seq, zorder=1, alpha = 0.3, color = "tab:blue")    
