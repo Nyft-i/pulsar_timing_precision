a0154579768aeba66cbfd1b6dfcfb3c42e143d94
lauren-alt <lauren.maclean@student.manchester.ac.uk>
2024-11-19 15:00:50 +0000

    final time real

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -586,7 +586,7 @@ def diff_plot_recovery():
     
     fig.suptitle(r'difference in retrieved $\Delta \nu$ and $\Delta \dot \nu$ and actual values', x=0.5, y=1.05)
     fig.supylabel(r'distance from true $\Delta \dot \nu$', y=0.5, x=0.06)
-    fig.supxlabel(r'distance from true $\Delta \nu$', y = -0.01)
+    fig.supxlabel(r'distance from true $\Delta \nu$', y = -0.2)
     
     axs[0].scatter(all_results_log[:,1]-master_traits[0], all_results_log[:,3]-master_traits[1], facecolors='none', edgecolors='tab:blue', s=all_results_log[:,7]*25, zorder=10, alpha = 0.3)
     axs[0].errorbar(all_results_log[:,1]-master_traits[0], all_results_log[:,3]-master_traits[1], xerr=all_results_log[:,2], yerr=all_results_log[:,4], fmt='x', label=seq, zorder=1, alpha = 0.3, color = "tab:blue")    
