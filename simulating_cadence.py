c9c721775e180b0629d3da4708f7acc226fd7aa4
lauren-alt <lauren.maclean@student.manchester.ac.uk>
2024-11-21 10:40:07 +0000

    type casting to string

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -409,8 +409,8 @@ def diff_plot():
     axs = gs.subplots(sharey = True, sharex = True)
     
     fig.suptitle(r'difference in retrieved $\Delta \nu$ and $\Delta \dot \nu$ and actual values', x=0.5, y=1.05)
-    fig.supylabel(r'$\Delta \dot \nu$ - ' + master_traits[1], y=0.5, x=0.06)
-    fig.supxlabel(r'$\Delta \nu$ - ' + master_traits[0], y = -0.13)
+    fig.supylabel(r'$\Delta \dot \nu$ - ' + str(master_traits[1]), y=0.5, x=0.06)
+    fig.supxlabel(r'$\Delta \nu$ - ' + str(master_traits[0]), y = -0.13)
     
     axs[0].scatter(all_results_log[:,1]-master_traits[0], all_results_log[:,3]-master_traits[1], facecolors='none', edgecolors='mediumorchid', s=all_results_log[:,7]*25, zorder=10, alpha = 0.3)
     axs[0].errorbar(all_results_log[:,1]-master_traits[0], all_results_log[:,3]-master_traits[1], xerr=all_results_log[:,2], yerr=all_results_log[:,4], fmt='x', label=seq, zorder=1, alpha = 0.3, color = "mediumorchid")    
