da77b936071461daae051017dbff384e6dd41514
Jade Salisbury <tom@salisbury-email.com>
2024-11-26 15:34:16 +0000

    ac10 w recovery periodic

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -672,10 +672,10 @@ def diff_plot_recovery():
     
 def data_output():
     #simulation params
-    seq = "geometric"
-    tim_iters = 90
-    sub_iters = 120
-    const = 1.6394
+    seq = "periodic"
+    tim_iters = 350
+    sub_iters = 30
+    const = 10
     
     #glitch params
     tim_name = "master_toas_exp.tim"
