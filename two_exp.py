ca032f8e16d43c1c5cce426f930b8e9db935a3a9
lauren-alt <lauren.maclean@student.manchester.ac.uk>
2024-11-26 16:17:36 +0000

    printing tempo2 command

--- two_exp.py
+++ two_exp.py
@@ -80,7 +80,7 @@ def run_fit(par, tim, recovery_mode = False):
     proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf8')
     out, err = proc.communicate()
     all_fields = out.split("\n")    
-#print(command)
+    print(command)
 
     f0, f0_e, f1, f1_e, ph, epochs, epoch_e, recovered_F0, recovered_F0_e, recovered_timescale, recovered_timescale_e, pulsar_f0, pulsar_f1, recovered_F0_2, recovered_F0_2_e, recovered_timescale_2, recovered_timescale_2_e = 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0                        
     
