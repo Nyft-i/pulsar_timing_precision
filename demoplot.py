fab6ac687bbf41c5fa9e998af5c5f0fb987faa12
Jade Salisbury <tom@salisbury-email.com>
2024-11-26 10:05:51 +0000

    added plotter code for a good examble of different observing cadences

--- demoplot.py
+++ demoplot.py
@@ -27,7 +27,7 @@ def event_maker(sequence_type, args):
     marker = start 
     while marker <= end:
         all_obs = np.append(all_obs, marker)
-        print("current cadence: ",cadence)
+        #print("current cadence: ",cadence)
 
         if sequence_type=='logarithmic': cadence = (np.log(1/10 * cadence + 1) * log_const) 
         elif sequence_type=='arithmetic': cadence = cadence + sequential_increase
@@ -46,37 +46,53 @@ def event_maker(sequence_type, args):
 fig = plt.figure(figsize=(7, 3))
 gs = fig.add_gridspec(3, hspace=1)
 axs = gs.subplots(sharex = True)
+
+args = (0.5, 0, 20, 2.3744)
+seq = 'arithmetic'
+print(tim_sampling.find_sequence_period_info(seq, args))
+
+
     
 args = (0.5, 0, 20, 5)
 seq = 'periodic'
+print(tim_sampling.find_sequence_period_info(seq, args))
+
 obs, ends = event_maker(seq, args)
 axs[0].set_xlim(0,140)
 axs[0].set_ylim(0.5,1)
 axs[0].set_yticks([])
-axs[0].eventplot(obs, linewidths=1, colors = "black", zorder=1, alpha=0.3)
-axs[0].eventplot(ends, colors="green", zorder=10)
+axs[0].set_title(seq, loc="left")
+axs[0].eventplot(obs, linewidths=1, colors = "black", alpha=0.3)
+axs[0].eventplot(ends, colors="green", zorder=5)
 
 args = (0.5, 0, 20, 25.7197)
 seq = 'logarithmic'
+print(tim_sampling.find_sequence_period_info(seq, args))
 obs, ends = event_maker(seq, args)
+toofar = obs[12] + 21.85
 axs[1].set_xlim(0,140)
 axs[1].set_ylim(0.5,1)
 axs[1].set_yticks([])
-axs[1].eventplot(obs, linewidths=1, colors = "black", zorder=1, alpha=0.3)
-axs[1].eventplot(ends, colors="green", zorder=10)
+axs[1].set_title(seq, loc="left")
+axs[1].eventplot(obs, linewidths=1, colors = "black", alpha=0.3)
+axs[1].axvline(x = toofar, linewidth=1, c = "red")
+axs[1].eventplot(ends, colors="green", zorder=5)
 
 args = (0.5, 0, 20, 1.6394)
 seq = 'geometric'
+print(tim_sampling.find_sequence_period_info(seq, args))
+
 obs, ends = event_maker(seq, args)
 axs[2].set_xlim(0,140)
 axs[2].set_ylim(0.5,1)
 axs[2].set_yticks([])
+axs[2].set_title(seq, loc="left")
 axs[2].eventplot(obs, linewidths=1, colors = "black", zorder=1, alpha=0.3)
-axs[2].eventplot(ends, colors="green", zorder=10)
+axs[2].eventplot(ends, colors="green", zorder=5)
 
-fig.supxlabel("days", y=-0.1)
+fig.supxlabel("days", y=-0.05)
 
-
+plt.savefig("demoplot.png", dpi=400, bbox_inches="tight")
 plt.show()
 
         
