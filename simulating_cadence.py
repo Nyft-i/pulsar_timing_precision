fcd742ab0ad8ee5cac6ab8446adf17cc699229c3
Jade Salisbury <tom@salisbury-email.com>
2024-11-26 15:22:17 +0000

    geo sim

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -672,10 +672,10 @@ def diff_plot_recovery():
     
 def data_output():
     #simulation params
-    seq = "logarithmic"
-    tim_iters = 140
-    sub_iters = 75
-    const = 25.7197
+    seq = "geometric"
+    tim_iters = 90
+    sub_iters = 120
+    const = 1.6394
     
     #glitch params
     tim_name = "master_toas_exp.tim"
