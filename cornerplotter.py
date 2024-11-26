f7c8cf9955c35d41fa47fb135f64a918953d9b82
lauren-alt <lauren.maclean@student.manchester.ac.uk>
2024-11-26 10:07:43 +0000

    geometric corner plot

--- cornerplotter.py
+++ cornerplotter.py
@@ -10,8 +10,8 @@ import corner as cn
 
 seq = "geometric"
 
-all_data = np.genfromtxt(seq+"_sim_data.txt", usecols=[1,3,8,13,15])
+all_data = np.genfromtxt("90_tims_120_sims_13_14.txt", usecols=[1,3,8,13,15])
 
-print(all_data)
+#print(all_data)
 
 cn.corner(all_data, labels=("glf0", "glf1", "epoch", "f0", "f1"))
