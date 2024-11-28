19d3adf5b4c0a4e67870cab078f27d3ce4cf4beb
Jade Salisbury <tom@salisbury-email.com>
2024-11-28 14:22:20 +0000

    15ac geo glitchb

--- simulating_cadence.py
+++ simulating_cadence.py
@@ -668,11 +668,11 @@ def diff_plot_recovery():
     
 def data_output():
     #simulation params
-    seq = "arithmetic"
+    seq = "geometric"
     tim_iters = 100
     sub_iters = 100
-    const = 4.33333
-    max_gap = 30
+    const = 1.66934
+    max_gap = 50
     start_cad = 2
     
     #glitch params
